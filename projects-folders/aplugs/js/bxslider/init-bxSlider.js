20c4e636184d902d5a0e301d7bdcf6fd756101e3
Roman Salko <maggotundead@yandex.ua>
2020-07-23 17:21:10 +0300

    ignore existing projects

--- projects-folders/aplugs/js/bxslider/init-bxSlider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.bxslider-two').bxSlider({
-        mode : 'fade',
-        auto : true,
-        pause: 6000,
-        speed: 2000
-        
-    });
-});
--- projects-folders/lookin-casino/php-version/access/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-// https://presentation-lookincasino.com/technical/logout/
-
-unlink($_SERVER['REMOTE_ADDR']);
-header('Location: https://demo-lookincasino.com/', true, 302);
