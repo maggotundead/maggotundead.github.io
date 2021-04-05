58bcf1329b65d8a9e8b524ed6db515ff15c73c97
Roman Salko <maggotundead@yandex.ua>
2021-04-06 02:21:53 +0300

    dent shop milestone

--- projects/dent-shop/app/js/scripts.js
+++ projects/dent-shop/app/js/scripts.js
@@ -476,6 +476,30 @@ $(document).ready(function () {
         slidesToShow: 1,
     });
 
+    $("#logo-slider").slick({
+        autoplay: true,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        variableWidth: true,
+    });
+
+    $("#actions-slider").slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 1,
+    });
+
+    $("#product-slider").slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 1,
+    });
+
     // document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     //     anchor.addEventListener("click", function (e) {
     //         e.preventDefault();
@@ -495,4 +519,91 @@ $(document).ready(function () {
             500
         );
     });
+
+    $(".product-item-info-toggler").on("click", function () {
+        $(this).closest(".product-item").toggleClass("open");
+    });
+
+    $(".open-read-more").on("click", function () {
+        $(this).closest(".text-container").addClass("open");
+    });
+
+    $(function () {
+        let $range = $(".js-range-slider"),
+            $from = $(".js-from"),
+            $to = $(".js-to"),
+            range,
+            min = 0,
+            max = $to.data("max"),
+            from,
+            to;
+
+        let updateValues = function () {
+            $from.prop("value", from);
+            $to.prop("value", to);
+        };
+
+        $range.ionRangeSlider({
+            type: "double",
+            min: min,
+            max: max,
+            prettify_enabled: false,
+            onChange: function (data) {
+                from = data.from;
+                to = data.to;
+
+                updateValues();
+            },
+        });
+
+        range = $range.data("ionRangeSlider");
+
+        let updateRange = function () {
+            range.update({
+                from: from,
+                to: to,
+            });
+        };
+
+        $from.on("change", function () {
+            from = +$(this).prop("value");
+            if (from < min) {
+                from = min;
+            }
+            if (from > to) {
+                from = to;
+            }
+
+            updateValues();
+            updateRange();
+        });
+
+        $to.on("change", function () {
+            to = +$(this).prop("value");
+            if (to > max) {
+                to = max;
+            }
+            if (to < from) {
+                to = from;
+            }
+
+            updateValues();
+            updateRange();
+        });
+    });
+
+    $(".description-tabs-selectors").on(
+        "click",
+        ".description-tab-selector",
+        function () {
+            $(this).addClass("active").siblings().removeClass("active");
+            $(this)
+                .closest(".description-tabs-wrapper")
+                .find(".description-tab")
+                .eq($(this).index())
+                .addClass("active")
+                .siblings()
+                .removeClass("active");
+        }
+    );
 });
