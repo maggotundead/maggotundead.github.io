5e187180023d1c99d4e46c164b132634c02a0e99
Roman Salko <maggotundead@yandex.ua>
2021-07-25 15:02:45 +0300

    pansionat beta

--- projects/tochka-oporu/gulpfile.js
+++ projects/tochka-oporu/gulpfile.js
@@ -10,7 +10,7 @@ const stylesDev = "./resources/sass/**/*.scss",
         // "./node_modules/slick-carousel/slick/slick.css",
         // "./node_modules/ion-rangeslider/css/ion.rangeSlider.min.css",
         // "./node_modules/animate.css/animate.min.css",
-        "./node_modules/swiper/swiper-bundle.min.css",
+        "./node_modules/swiper/swiper-bundle.css",
         // './resources/sass/**/*.scss'
         "./resources/sass/**/*.scss",
     ],
--- projects/tochka-oporu/resources/js/main.js
+++ projects/tochka-oporu/resources/js/main.js
@@ -2,4 +2,62 @@ $(function () {
     $('.accordeon-item').on('click', function() {
         $(this).toggleClass('open').siblings().removeClass('open');
     });
+
+    const inviteSliderThumbs = new Swiper('.invite-slider-thumbs', {
+        // centeredSlides: true,
+        // centeredSlidesBounds: true,
+        slidesPerView: 3,
+        // watchOverflow: true,
+        spaceBetween: 15,
+        // loop: true,
+        // watchSlidesVisibility: true,
+        // watchSlidesProgress: true,
+        // direction: 'vertical',
+
+        breakpoints: {
+            768: {
+                direction: 'vertical'
+            },
+        }
+    });
+    const inviteSliderGallery = new Swiper('.invite-slider-gallery', {
+        // watchOverflow: true,
+        // watchSlidesVisibility: true,
+        // watchSlidesProgress: true,
+        // preventInteractionOnTransition: true,
+        // loop: true,
+        navigation: {
+            nextEl: '#invite-slide-next',
+            prevEl: '#invite-slide-prev',
+        },
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        thumbs: {
+            swiper: inviteSliderThumbs
+        },
+        pagination: {
+            el: '#invite-pagination',
+			clickable: true,
+        },
+    });
+    inviteSliderGallery.on('slideChangeTransitionStart', function() {
+        inviteSliderThumbs.slideTo(inviteSliderGallery.activeIndex);
+    });
+    inviteSliderThumbs.on('transitionStart', function() {
+        inviteSliderGallery.slideTo(inviteSliderThumbs.activeIndex);
+    });
+
+    $('.modal-btn').on('click', function() {
+        $('.modal').addClass('open');
+    });
+
+    $('.modal-overlay, .modal-close').on('click', function() {
+        $('.modal').removeClass('open');
+    });
+
+    $('.burger, .mobile-popup-close').on('click', function() {
+        $('.mobile-popup').toggleClass('open');
+    });
 });
