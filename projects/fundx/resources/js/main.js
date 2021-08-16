8c2a66f68e90b09180de0c6aecb563dd682c02b5
Roman Salko <maggotundead@yandex.ua>
2021-08-16 19:33:26 +0300

    fix styles responsive & modal

--- projects/fundx/resources/js/main.js
+++ projects/fundx/resources/js/main.js
@@ -1,6 +1,14 @@
 $(function () {
-    $(".modal-overlay, .modal-close").on("click", function () {
-        $(".modal").removeClass("open");
+
+    $(document).on("click", 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $("html, body").animate(
+            {
+                scrollTop: $($.attr(this, "href")).offset().top,
+            },
+            500
+        );
     });
 
     let header = $(".header");
@@ -13,16 +21,41 @@ $(function () {
         else header.removeClass("fixed");
     });
 
+    $('[data-trade-program]').on('click', function(e) {
+        e.preventDefault();
+        let contractBlockId = $(this).attr('data-trade-program');
 
-    $(document).on("click", 'a[href^="#"]', function (event) {
-        event.preventDefault();
+        $('body').addClass('modal-open');
+        $('#modal-contract').addClass('open');
+        $(`#trade-program-${contractBlockId}`).addClass('open');
 
-        $("html, body").animate(
-            {
-                scrollTop: $($.attr(this, "href")).offset().top,
-            },
-            500
-        );
+        $('.modal-overlay, .modal-close').on('click', function () {
+            $('body').removeClass('modal-open');
+            $('.contract-order').removeClass('open');
+        });
+    });
+
+    $('[data-modal-login]').on('click', function () {
+        $('body').addClass('modal-open');
+        $('.modal').removeClass('open');
+        $('#modal-login').addClass('open');
+    });
+
+    $('[data-modal-register]').on('click', function () {
+        $('body').addClass('modal-open');
+        $('.modal').removeClass('open');
+        $('#modal-register').addClass('open');
+    });
+
+    $('[data-modal-reset-password]').on('click', function () {
+        $('body').addClass('modal-open');
+        $('.modal').removeClass('open');
+        $('#modal-reset-password').addClass('open');
+    });
+
+    $(".modal-overlay, .modal-close").on("click", function () {
+        $('body').removeClass('modal-open');
+        $(".modal").removeClass("open");
     });
 
 });
